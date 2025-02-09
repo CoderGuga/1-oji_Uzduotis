2f0df3a78ca35c326a4d086d49c1ba20993cf346
Gabrielius Rutkauskas <gabrielius.rut@gmail.com>
2025-02-09 10:36:14 +0200

    Add files via upload
    papildytas helper failas

--- helper.h
+++ helper.h
@@ -16,11 +16,15 @@ using std::endl;
 using std::string;
 using std::vector;
 
+const int MAX_GRADES = 10;
+
 struct Stud {
     std::string pavarde = "Pavardenis";
     std::string vardas = "Vardenis";
     int egz = 10;
-    std::vector<int> nd;
+    vector <int> ndVector;
+    int nd[MAX_GRADES];
+    int nd_count = 0;
 };
 
 template <typename T>
@@ -37,5 +41,25 @@ std::optional<T> string_to(const std::string & s)
 
 int TypeInt(const std::string& text, int max = 0);
 std::string TypeString(const std::string& prompt);
+
+
+string GenName()
+{
+    const std::string names[] = {"Jonas", "Petras", "Antanas", "Kazys", "Vytautas", "Tomas", "Marius", "Andrius", "Darius", "Saulius"};
+    int index = rand() % (sizeof(names) / sizeof(names[0]));
+    return names[index];
+}
+
+string GenSurname()
+{
+    const std::string surnames[] = {"Jonaitis", "Petraitis", "Antanaitis", "Kazlauskas", "Takelis", "Tomaitis", "Andraitis"};
+    int index = rand() % (sizeof(surnames) / sizeof(surnames[0]));
+    return surnames[index];
+}
+
+int RandInt(int min, int max)
+{
+    return rand() % (max - min + 1) + min;
+}
 
 #endif // HELPER_H
